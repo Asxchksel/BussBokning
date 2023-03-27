ac52ff91a67053b3f0a3a6fd59ce2c220da08ff9
axel.mannheimer <axel.mannheimer@5CD132CZP9.learnet.se>
2023-03-27 13:27:47 +0200

    nytt

--- src/buss_system/Buss_System.java
+++ src/buss_system/Buss_System.java
@@ -8,43 +8,55 @@ public class Buss_System {
             
     public static void main(String[] args) {
         System.out.println("Vad vill du göra?:");
-        System.out.println("1. Lägg till passagerare");
-        System.out.println("2. Kolla lediga platser");
-        System.out.println("3. Beräkna vinst");
-        System.out.println("4. Avsluta programmet");
-        
+ 
         String[] fält = new String[5];
             fält[1] = "1. Lägg till passagerare";
             fält[2] = "2. Kolla lediga platser";
             fält[3] = "3. Beräkna vinst";
             fält[4] = "4. Avsluta programmet";
-        
+            
+        for (int i = 0; i < 5; i++) {
+            System.out.println(fält[i]);
+        }
+            
         byte menyval = keyboard.nextByte();
+        boolean menyboolean = true;
         
-        while(menyval = 1->4){
+        while(menyboolean == true){
             switch(menyval){
                 case 1:
                     läggtillpassagerare();
+                    menyboolean = false;
                 case 2:
                     ledigaplatser();
+                    menyboolean = false;
                 case 3:
                     räknavinst();
+                    menyboolean = false;
                 case 4:
                     System.out.println("Okej, Hejdå!");
+                    menyboolean = false;
                     break;
                 default:  
                     System.out.println("Välj ett tal mellan 1-4");
+
     }}
     }
         
         static void läggtillpassagerare(){
             System.out.println("Skriv in ditt personnummer i formatet, yyyymmdd");
-            int personnnummer = keyboard.nextInt();
-            
+            int personnummer = keyboard.nextInt();
+            if(personnummer > 20050101){
+                barnplats = (byte) (barnplats +1);
+            }
+            if(personnummer > 19541231){
+                
+            }
+
         }
     
         static void ledigaplatser(){
-            
+            System.out.println("hej");
         }
         
         static int räknavinst(){
